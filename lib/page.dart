06182751192f9aa6e16de239d63f30fe479d451b
zzq <i@zhangzqs.cn>
2022-05-31 17:53:54 +0800

    feat: 优化代码结构

---
+++ lib/page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import 'abstract.dart';
+import 'component.dart';
+import 'entity.dart';
+import 'future_builder.dart';
+
+class SearchBodyWidget extends StatefulWidget {
+  final ASearcher searcher;
+  const SearchBodyWidget(this.searcher, {Key? key}) : super(key: key);
+
+  @override
+  State<SearchBodyWidget> createState() => _SearchBodyWidgetState();
+}
+
+class _SearchBodyWidgetState extends State<SearchBodyWidget> {
+  String query = '';
+  int pageId = 1;
+  int limitSize = 100;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SearchInputWidget(
+          startText: query,
+          callback: (val) async {
+            setState(() {
+              query = val;
+              pageId = 1;
+              limitSize = 100;
+            });
+          },
+        ),
+        Expanded(
+          child: MyFutureBuilder<SearchResponse>(
+            future: widget.searcher.search(
+              query,
+              pageId,
+              limit: limitSize,
+            ),
+            builder: (BuildContext context, data) {
+              return Column(
+                children: [
+                  Expanded(child: SearchResultWidget(data)),
+                  if (data.total != 0)
+                    PageSwitcher(
+                      currentPage: data.page,
+                      totalPage: data.totalPage,
+                      onPageSwitch: (int value) {
+                        setState(() {
+                          pageId = value;
+                        });
+                      },
+                    ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  final ASearcher searcher;
+  const SearchPage(this.searcher, {Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("搜索引擎"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SearchBodyWidget(searcher),
+      ),
+    );
+  }
+}
